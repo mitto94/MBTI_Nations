433612f43d3cdcf31f3bf5c4d02886225471d245
eunchong <21300307@handong.edu>
2022-05-10 17:41:30 +0900

    mob version

--- src/ResultModal.jsx
+++ src/ResultModal.jsx
@@ -9,15 +9,14 @@ const ResultModal = ({ isOpen, onSubmit, onCancel, result }) => {
     onCancel();
   };
   if (!result) return null;
-  console.log("resu", result);
 
   return (
     <ReactModal isOpen={isOpen}>
       <div id={`${result}`} style={{width: "100%", height: "90%"}}></div>
-      <div style={{fontSize: "1vw", opacity: 0.05, position: "absolute"}}>{result}</div>
+      <div style={{fontSize: "1vw", opacity: 0.1, position: "absolute"}}>{result}</div>
      
       <div className="gyeonggi" style={{height: "10%", width: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
-        <a onClick={handleClickSubmit} style={{fontSize: "1.75vw", color: "#73788F", textDecoration: "underline", textUnderlinePosition: "under", cursor: "pointer"}}>처음으로</a>
+        <a className='mobFont' onClick={handleClickSubmit} style={{fontSize: "1.75vw", color: "#73788F", textDecoration: "underline", textUnderlinePosition: "under", cursor: "pointer"}}>처음으로</a>
       </div>
     </ReactModal>
   );
